e15d411185076d59a332690dafaf1d7fdc70e3e9
HenriARM <henriarm@yandex.ru>
2019-06-27 10:55:11 +0300

    Updated Navbar and commented unused routes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -24,8 +24,8 @@
 
             <ul class="nav navbar-nav">
               <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/services">Services</a></li>
+<!--              <li><a href="/about">About</a></li>-->
+<!--              <li><a href="/services">Services</a></li>-->
               <li><a href="/posts">Blog</a></li>
               <li><a href="/localization/en">EN</a></li>
               <li><a href="/localization/lv">LV</a></li>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::get('/', 'PagesController@index');
-Route::get('/about', 'PagesController@about');
-Route::get('/services', 'PagesController@services');
+//Route::get('/about', 'PagesController@about');
+//Route::get('/services', 'PagesController@services');
 Route::get('localization/{locale}', 'PagesController@changeLang');
 Route::resource('posts', 'PostsController');
 Auth::routes();
